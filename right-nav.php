5b0a56f92b90ed630477ec26d219eb2d73bc777c
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-05-28 00:32:49 -0400

    Woops, here's right-nav.php

---
+++ right-nav.php
@@ -0,0 +1,26 @@
+<div class="span3">
+    <div class="well" style="padding: 8px 0; background-color: #FBFBFB;">
+    <ul class="nav nav-list">
+        <li class="nav-header"> 
+        Links
+        </li>
+    
+        <li><a href="http://www.qc.cuny.edu/Pages/home.aspx">Queens Collge</a></li>
+        <li><a href="http://www.cs.qc.cuny.edu/index-5.html">Queens College CS</a></li>
+        <li><a href="http://flushingchamber.com">Flushing Chamber of Commerce</a></li>
+    </ul>
+    </div>
+
+
+    <div class="social">
+    <div class="well" style="padding: 8px 0; background-color: #FBFBFB;">
+    <ul class="nav nav-list">
+        <li class="nav-header"> 
+        Social
+        </li>
+        <li><a href="https://github.com/">github</a></li>
+    </ul>
+    </div>
+    </div>
+
+</div>
