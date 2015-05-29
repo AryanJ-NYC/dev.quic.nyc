304ad514a70595fce696c027b9c97a94b3f3f6db
carlosdelros <carlosdelros@gmail.com>
2015-05-28 23:51:52 -0400

    organized html, moved relocated css link, rewrote h tags,etc

--- index.php
+++ index.php
@@ -1,107 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
+  <head>
+    <meta charset="utf-8" />
     <title>Queens College Incubator</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
     <!--[if lt IE 9]>
     <script src="/theme/html5.js"></script>
     <![endif]-->
-
     <!-- Le styles -->
-    <link href="/theme/bootstrap.min.css" rel="stylesheet">
-    <link href="/theme/bootstrap.min.responsive.css" rel="stylesheet">
-    <link href="/theme/local.css" rel="stylesheet">
-    <link href="/theme/pygments.css" rel="stylesheet">
-
+    <link href="/theme/bootstrap.min.css" rel="stylesheet" />
+    <link href="/theme/bootstrap.min.responsive.css" rel="stylesheet" />
+    <link href="/theme/local.css" rel="stylesheet" />
+    <link href="/theme/pygments.css" rel="stylesheet" />
     <!-- So Firefox can bookmark->"abo this site" -->
-
-</head>
-
-<body>
-
-<?php include 'header.php'; ?>
-
-<div class="container">
+  </head>
+  <body><?php include 'header.php'; ?>
+  <div class="container">
     <div class="content">
-    <div class="row">
-
+      <div class="row">
         <div class="span9">
-        
-
-        
-
-
-    <div class='article'>
-        <div class="content-title">
-            <a href="/Fund-Announcement.html"><h1>Funding Announcement</h1></a>
-Mon 28 July 2014
-
-        </div>
-        
-        <div><p>Governor Andrew M. Cuomoo today announced the first round of NY CUNY
-2020 awards| providing $55 million to fund eight innovative projects
-involving 20 participating CUNY campuses designed to connect academic
-excellence with entrepreneurship and local economic
-development. Projects were selected through a competitive process
-utilizing staff from CUNY and Empire State Development and will
-support the creation of more than 3|800 jobs over six years.</p>
-<p>"The students at New York City’s public colleges and universities have
-provided the world with game-changing ideas in fields ranging from
-healthcare to manufacturing. Through the CUNY2020 program we are
-maximizing those ideas to create economic opportunity for New
-Yorkers,” Governor Cuomo said. “The first round of CUNY 2020 projects
-will help link some of our best and brightest students with positions
-in high-tech sectors, while leveraging their academic excellence to
-support economic development and create new opportunities throughout
-the New York City area. By funding these eight projects we are making
-an investment in New York’s future, and I am proud to present these
-awards to the first round of CUNY 2020 recipients.”</p>
-<p>CUNY Chancellor James B. Milliken said, “We commend Governor Cuomo and
-the State of New York for investing in The City University of New York
-with CUNY 2020. CUNY has developed and implemented many public/private
-partnerships and this vitally important initiative will continue to
-grow the economy, create employment opportunities, and expand our
-research and development in all five boroughs. This is a great day for
-the University and all New Yorkers.”</p>
-<p>The NY CUNY 2020 program links the knowledge and innovation of higher
-education to regional economic revitalization, and the Governor
-appropriated $55 million as part of the 2013-14 State Budget for NY
-CUNY 2020. The program offers grants for 2- and 4-year colleges within
-the CUNY system for projects that connect economic development and
-academic excellence. Projects are selected in a competitive manner
-based on economic impact, advancement of academic goals, innovation,
-and collaboration.</p>
-<p>Advanced Manufacturing for Economic Development (awarded $1.5
-million): Queensborough Community College; Queens College</p>
-<p>This project will develop an Advanced Manufacturing Center focused on
-3-D printing, to be renovated and equipped within 6 months with 3-D
-printing equipment. Students will gain the skills to design and
-produce sophisticated components through collaboration with the Queens
-College Computer Science Department and local high schools, which will
-create new courses for students to learn sophisticated program and
-parts processes.</p>
-<p>3-D printing is a technological process that can considerably lower
-manufacturing costs and return companies to the domestic
-market. Manufacturers need workers, as several hundred graduates over
-a 5 year period will be produced in this emerging technology,
-manufacturers will have increased capability to produce parts
-domestically that are currently developed overseas.</p></div>
-        <hr />
-    </div>
-		
-        </div>
-        
-<?php include('right-nav.php') ?>
-
-    </div>     </div> 
-<?php include('footer.php') ?>
-
-</div> <!-- /container -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script src="/theme/bootstrap-collapse.js"></script>
- 
-</body>
+          <div class='article'>
+            <div class="content-title">
+            <a href="/Fund-Announcement.html">
+              <h1>Funding Announcement</h1>
+            </a> Mon 28 July 2014</div>
+            <div>
+              <p>Governor Andrew M. Cuomoo today announced the first round of NY CUNY 2020 awards| providing $55 million to fund
+              eight innovative projects involving 20 participating CUNY campuses designed to connect academic excellence with
+              entrepreneurship and local economic development. Projects were selected through a competitive process utilizing staff
+              from CUNY and Empire State Development and will support the creation of more than 3|800 jobs over six years.</p>
+              <p>&quot;The students at New York City’s public colleges and universities have provided the world with
+              game-changing ideas in fields ranging from healthcare to manufacturing. Through the CUNY2020 program we are
+              maximizing those ideas to create economic opportunity for New Yorkers,” Governor Cuomo said. “The first round of
+              CUNY 2020 projects will help link some of our best and brightest students with positions in high-tech sectors, while
+              leveraging their academic excellence to support economic development and create new opportunities throughout the New
+              York City area. By funding these eight projects we are making an investment in New York’s future, and I am proud to
+              present these awards to the first round of CUNY 2020 recipients.”</p>
+              <p>CUNY Chancellor James B. Milliken said, “We commend Governor Cuomo and the State of New York for investing in
+              The City University of New York with CUNY 2020. CUNY has developed and implemented many public/private partnerships
+              and this vitally important initiative will continue to grow the economy, create employment opportunities, and expand
+              our research and development in all five boroughs. This is a great day for the University and all New Yorkers.”</p>
+              <p>The NY CUNY 2020 program links the knowledge and innovation of higher education to regional economic
+              revitalization, and the Governor appropriated $55 million as part of the 2013-14 State Budget for NY CUNY 2020. The
+              program offers grants for 2- and 4-year colleges within the CUNY system for projects that connect economic
+              development and academic excellence. Projects are selected in a competitive manner based on economic impact,
+              advancement of academic goals, innovation, and collaboration.</p>
+              <p>Advanced Manufacturing for Economic Development (awarded $1.5 million): Queensborough Community College; Queens
+              College</p>
+              <p>This project will develop an Advanced Manufacturing Center focused on 3-D printing, to be renovated and equipped
+              within 6 months with 3-D printing equipment. Students will gain the skills to design and produce sophisticated
+              components through collaboration with the Queens College Computer Science Department and local high schools, which
+              will create new courses for students to learn sophisticated program and parts processes.</p>
+              <p>3-D printing is a technological process that can considerably lower manufacturing costs and return companies to
+              the domestic market. Manufacturers need workers, as several hundred graduates over a 5 year period will be produced
+              in this emerging technology, manufacturers will have increased capability to produce parts domestically that are
+              currently developed overseas.</p>
+            </div>
+            <hr />
+          </div>
+        </div><?php include('right-nav.php') ?>
+      </div>
+    </div><?php include('footer.php') ?>
+  </div>
+  <!-- /container -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> 
+  <script src="/theme/bootstrap-collapse.js"></script></body>
 </html>
--- research.php
+++ research.php
@@ -1,61 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
+  <head>
+    <meta charset="utf-8" />
     <title>Queens College Incubator</title>
-    <meta name="description" content="">
-    <meta name="author" content="Queens College Incubator">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <meta name="description" content="" />
+    <meta name="author" content="Queens College Incubator" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
     <!--[if lt IE 9]>
     <script src="/theme/html5.js"></script>
     <![endif]-->
-
     <!-- Le styles -->
-    <link href="/theme/bootstrap.min.css" rel="stylesheet">
-    <link href="/theme/bootstrap.min.responsive.css" rel="stylesheet">
-    <link href="/theme/local.css" rel="stylesheet">
-    <link href="/theme/pygments.css" rel="stylesheet">
-
+    <link href="/theme/bootstrap.min.css" rel="stylesheet" />
+    <link href="/theme/bootstrap.min.responsive.css" rel="stylesheet" />
+    <link href="/theme/local.css" rel="stylesheet" />
+    <link href="/theme/pygments.css" rel="stylesheet" />
     <!-- So Firefox can bookmark->"abo this site" -->
-
-</head>
-
-<body>
-
-<?php include 'header.php'; ?>
-
-<div class="container">
+  </head>
+  <body><?php include 'header.php'; ?>
+  <div class="container">
     <div class="content">
-    <div class="row">
-
+      <div class="row">
         <div class="span9">
-        
-<section id="content" class="body">    
-    <h2 class="content-title">
-        Research
-    </h2>
-    <h1>Building performance</h1>
-<p>In partnership with the building performance lab at the CUNY School of Professional Studies 
-and the CUNY Instituute of Software Design and Development the Queens Incubator is 
-applying machine learning to the prediction of tempratures and energy consumption of 
-large buildings.  Every year, New York City Government (and its residents) spend literally 
-billions of dollars to heat and cool their living and work space. Sadly much of that spending
-is wasteful with buildings being heated and cooled at the same time.  Our project hopes to 
-guide New York City agencies on how to make their workspace more comfortable in an energy 
-efficient manner.</p>
-</section>
-        </div>
-        
-    <?php include('right-nav.php') ?>
-    </div>     </div> 
-    
-<?php include('footer.php') ?>
-
-</div> <!-- /container -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script src="/theme/bootstrap-collapse.js"></script>
- 
-</body>
+          <section id="content" class="body">
+            <h1 class="content-title">Research</h1>
+            <h2>Building performance</h2>
+            <p>In partnership with the building performance lab at the CUNY School of Professional Studies and the CUNY Instituute
+            of Software Design and Development the Queens Incubator is applying machine learning to the prediction of tempratures
+            and energy consumption of large buildings. Every year, New York City Government (and its residents) spend literally
+            billions of dollars to heat and cool their living and work space. Sadly much of that spending is wasteful with
+            buildings being heated and cooled at the same time. Our project hopes to guide New York City agencies on how to make
+            their workspace more comfortable in an energy efficient manner.</p>
+          </section>
+        </div><?php include('right-nav.php') ?>
+      </div>
+    </div><?php include('footer.php') ?>
+  </div>
+  <!-- /container -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> 
+  <script src="/theme/bootstrap-collapse.js"></script></body>
 </html>
