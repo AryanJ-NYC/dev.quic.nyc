50691403da8b379d16a4d545712bd608f1dbb658
Kevin Ramsunder <KevinRamsunder5@gmail.com>
2015-06-03 03:24:17 -0400

    Added success landing page for form.. work in progess

---
+++ success.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Queens College Incubator</title>
+    <meta name="description" content="" />
+    <meta name="author" content="Queens College Incubator" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+    <script src="/theme/html5.js"></script>
+    <![endif]-->
+    <!-- Le styles -->
+    <link href="./theme/bootstrap.min.css" rel="stylesheet" />
+    <link href="./theme/bootstrap.min.responsive.css" rel="stylesheet" />
+    <link href="./theme/local.css" rel="stylesheet" />
+    <link href="./theme/pygments.css" rel="stylesheet" />
+    <!-- So Firefox can bookmark->"abo this site" -->
+  </head>
+  <body><?php include 'header.php'; ?>
+  <div class="container">
+    <div class="content">
+      <div class="row">
+        <div class="span9">
+          <section id="content" class="body">
+            <center>
+              <h1 class="content-title">Success!</h1>
+              <h3>Work in progress...</h3>
+            </center>
+          </section>
+    </div><?php include('footer.php') ?>
+  </div>
+  <!-- /container -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> 
+  <script src="./theme/bootstrap-collapse.js"></script></body>
+</html>
