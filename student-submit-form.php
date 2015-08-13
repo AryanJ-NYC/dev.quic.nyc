204c7458d8375b2191309899761a653500d33b7c
Kevin Ramsunder <KevinRamsunder5@gmail.com>
2015-08-12 22:30:55 -0400

    Added PHP code to save form data to local database

--- student-submit-form.php
+++ student-submit-form.php
@@ -2,6 +2,7 @@
 # PHP Mailer library for attachments
 require_once './assets/libraries/PHPMailer/PHPMailerAutoload.php';
 require_once './assets/libraries/reCAPTCHA/recaptchalib.php';
+require_once("database.php");
 
 # get the value from the input field
 function get($name) {
@@ -56,7 +57,8 @@ if (!$resp->is_valid) {
 }
 
 # to and from fields for the email
-$to = "TBrown@gc.cuny.edu"; 
+#$to = "TBrown@gc.cuny.edu"; 
+$to = "kevinramsunder4@gmail.com"; 
 $from = "info@quic.nyc";
 
 # fields on the form
@@ -72,17 +74,35 @@ $fields = array(
     "credstograd" => "Credits to Graduate:",
     "lb2" => "linebreak",
     "programmingExperience" => "Programming Experience:",
-    "personalstatement" => "Personal Statement",
+    "personalstatement" => "Personal Statement:",
     "lb5" => "linebreak"
 );
 
 # get message and error values
 $errorValues = array();
 $message = generateMessage($fields, $errorValues);
+$query = "INSERT INTO `student`(`Name`, `Email`, `Phone`, `Degree`, `Graduation`, `Year`, `Status`, `Credits_Needed`, `experience`, `personal`) VALUES ('%NAME%','%EMAIL%','%PHONE%','%DEGREE%','%GRAD%','%YEAR%','%STATUS%','%CREDITS%','%EXP%','%PERSONAL%')";
    
 # if no fields are empty, send the email. 
 # echo result to AJAX script
 if(count($errorValues) == 0) {
+    $query = str_replace("%NAME%", get('applicant'), $query);
+    $query = str_replace("%EMAIL%", get('email'), $query);
+    $query = str_replace("%PHONE%", get('phone'), $query);
+    $query = str_replace("%DEGREE%", get('degree'), $query);
+    $query = str_replace("%GRAD%", get('gradseason'), $query);
+    $query = str_replace("%YEAR%", get('gradyear'), $query);
+    $query = str_replace("%STATUS%", get('citizenship'), $query);
+    $query = str_replace("%CREDITS%", get('credstograd'), $query);
+    $query = str_replace("%EXP%", get('programmingExperience'), $query);
+    $query = str_replace("%PERSONAL%", get('personalstatement'), $query);
+
+    if ($db->query($query) === TRUE) {
+        $message .= "\n***\nThis information has been successfully added to database table [student]";
+    } else {
+        $message .= "\n***\nThere was an error saving this information to the database. Please keep a copy of this email for your records.";
+    }
+
     $subject = "QC Incubator: " . get('applicant') . " Form Submission";
     sendEmail($to, $subject, $message, $from);
     echo ("success");
