8f59c7acf7f56c5840770f0a24b94e54efc78d41
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-08-13 18:13:17 -0400

    Added calendar to events.php using upto

---
+++ events.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"    content="width=device-width, initial-scale=1.0">
+    
+    <title>Resources</title>
+
+    <link rel="shortcut icon" href="assets/images/gt_favicon.ico" type="image/x-icon"/>
+
+    <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <!-- Custom styles for our template -->
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <!-- Fixed navbar -->
+    <?php include("./navbar.php"); ?>
+
+    <!-- container -->
+    <div class="container">
+
+        <ol class="breadcrumb">
+            <li><a href="index.php">Home</a></li>
+            <li class="active">Resources</li>
+        </ol>
+
+        <div class="row">     
+            <!-- Article main content -->
+            <div class="maincontent">
+                <header class="page-header"><h1 class="page-title">Events at the Queens College Tech Incubator</h1></header>
+                <!-- Embed code for upto calendar -->
+                <div id="upto-calendar-container-mBLO" class="upto-calendar-plugin"><script>!function(c,b,a){function d(h){var e=document.getElementById("upto-calendar-container-"+c),f=document.createElement("SCRIPT"),g="https://upto.com/js/plugin.js";f.onreadystatechange=function(){if(f.readyState==="loaded"||f.readyState==="complete"){f.onreadystatechange=null;h()}};f.onload=function(){h()};f.setAttribute("src",g);e.parentNode.insertBefore(f,e)}d(function(){upto.init(c)})}("mBLO");</script>
+                </div>
+            </div> <!-- /.maincontent -->     
+        </div>
+    </div>  <!-- /container -->
+    
+
+    <!-- footer -->
+    <?php include("./footer.php"); ?>
+
+    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="assets/js/headroom.min.js"></script>
+    <script src="assets/js/jQuery.headroom.min.js"></script>
+    <script src="assets/js/template.js"></script>
+</body>
+</html>
