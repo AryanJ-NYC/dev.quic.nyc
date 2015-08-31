c10e22ff5ccb9bc0ac441ed2bb09e00da293f590
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-08-31 14:15:06 -0400

    Replaced lorem ipsum with Under Construction image

--- incubatorspace.php
+++ incubatorspace.php
@@ -57,7 +57,8 @@
             <div class="col-lg-12">
                 <h1 class="page-header">Incubator Center</h1>
             </div>
-
+            <img src="http://www.mgei-iagi.org/UnderConstruction1.png">
+            <!--
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
                     <img class="img-responsive" src="http://placehold.it/400x300" alt="">
@@ -117,7 +118,7 @@
                 <a class="thumbnail" href="#">
                     <img class="img-responsive" src="http://placehold.it/400x300" alt="">
                 </a>
-            </div>
+            </div>-->
         </div>
     </div>  <!-- /container -->
     
