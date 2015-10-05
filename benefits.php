0140aa62e0f249943416dc3f88967e1f55be5d9a
carlosdelros <carlosdelros@gmail.com>
2015-10-05 17:21:50 -0400

    Added content to the benefit.php page

--- benefits.php
+++ benefits.php
@@ -33,7 +33,16 @@
             <header class="page-header"><h1 class="page-title">Member Benefits</h1></header>
             <!-- Article main content -->
             <article class="col-sm-8 maincontent">
-                <img src="http://www.mgei-iagi.org/UnderConstruction1.png">
+                <p>Whether you’re an independent app developer, or a team that works on full-time projects, you can leverage QUIC resources to enhance your software development. We’re seeking new members for our incubator space. We offer different levels of membership on a monthly or annual basis each including a range of benefits.</p>
+				
+				<ul>
+				<li>Approved members for private offices will be offered 24/7 access to their preferable private office.  </li>
+				<li>Approved members for shared offices will be offered 24/7 access to shared offices on a first-come, first-served basis. </li>
+				<li>Invitation to our master classes or workshops with up to 25% discounts.</li>
+				<li>All approved members are eligible for applying for parking on campus. The parking space is limited and subject to QC parking rules.</li>
+				<li>Access to our facility, lounge room, resource center, and most IT resources.  </li>
+				<li>On-site administrative and IT staff assistance</li>
+				</ul>
             </article>
 
             <aside class="col-sm-4 sidebar sidebar-right">
