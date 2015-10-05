184ad346d607bac9e0d4231b6cdf9402e2b6789d
carlosdelros <carlosdelros@gmail.com>
2015-10-05 16:28:01 -0400

    Added content to the IT page

--- it.php
+++ it.php
@@ -33,7 +33,23 @@
             <header class="page-header"><h1 class="page-title">Information Technology Resources</h1></header>
             <!-- Article main content -->
             <article class="col-sm-8 maincontent">
-                <img src="http://www.mgei-iagi.org/UnderConstruction1.png">
+                
+				<ul>
+				<li> <b>Servers:</b>
+					<P>We have two new and powerful servers: Dell PowerEdge R730 and Dell PowerEdge R920. Especially, R920 is configured two E7-8880L 2.2GHz Turbo 15 core CPUs, 1TB Memory, and 43TB Hard Disk. The dedicated building (CEP2) is equipped with 4 Cisco Catalyst 4500E Series switches, which would provide all Ethernet ports in building. While QC campus has average WiFi density and coverage, we deploy additional Cisco AP that provides dense coverage with high speed wireless connection. Virtualization is deployed via Citrix Xen server that provide virtual hosts on R730 and R920. Default applications on our servers include LAMP server, Hadoop, MySQL, PostgreSQL, Mongo DB, Subversion server, and more. Security is our top priority in servers and the network. While QUIC maintains our own sub network, we strictly follow Queens College security policy, and all operating network traffic is protected by a firewall.</P>
+					
+				<li> <b>App Center:</b>
+				<P>	App Center is mainly utilized by our QUIC fellows/interns who are talented computer science students on campus. We equip an advanced 70-inch interactive flat panel display with collaborative capability via ShareLink 200N. 27-inch iMacs are the main desktop workforce for App Center.</P>
+				
+				<li> <b>Conference System:</b>
+				<!--(more goes here, cite specs from A/V quote)-->
+				
+				<li> <b>Workshop/Classroom:</b>
+				<!--(more goes here, cite specs from A/V quote)-->
+				
+				<li> <b>Resource Center:</b>
+				<P>QUIC provides network copier, 3D printer in the dedicated resource center for all members.</P>
+				</ul>
             </article>
 
             <aside class="col-sm-4 sidebar sidebar-right">
